6c2af5ac484bfe91f4eb4bc15c2c94d561f78379
Egor Fedorov <fegor2004@gmail.com>
2024-11-18 21:07:36 +0300

    add principal block to sidebar

--- frontend/src/app/layout/layout.tsx
+++ frontend/src/app/layout/layout.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "@tanstack/react-router";
+import { PrincipalSidebarFooter } from "@/entities/principal";
 import { AppSidebar } from "@/shared/components/app-sidebar.tsx";
 import {
   SidebarInset,
@@ -9,7 +10,7 @@ import {
 export const Layout = () => {
   return (
     <SidebarProvider>
-      <AppSidebar />
+      <AppSidebar principalSlot={<PrincipalSidebarFooter />} />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger />
---
+++ frontend/src/entities/principal/ui/sidebar.tsx
@@ -0,0 +1,115 @@
+import { useSuspenseQuery } from "@tanstack/react-query";
+import {
+  BadgeCheck,
+  Bell,
+  ChevronsUpDown,
+  CreditCard,
+  LogOut,
+  Sparkles,
+} from "lucide-react";
+import { getPrincipalQueryOptions } from "@/entities/principal";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/shared/components/ui/avatar.tsx";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/shared/components/ui/dropdown-menu";
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/shared/components/ui/sidebar";
+
+export function PrincipalSidebarFooter() {
+  const { data } = useSuspenseQuery(getPrincipalQueryOptions);
+  const { isMobile } = useSidebar();
+
+  const username = data.username;
+  const fullName = "Egor Fedorov";
+  const avatarFallback = fullName
+    .split(" ")
+    .map((word) => word[0])
+    .reduce((a, b) => a + b, "");
+
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
+              <Avatar className="h-8 w-8 rounded-lg">
+                <AvatarFallback className="rounded-lg">
+                  {avatarFallback}
+                </AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-semibold">{fullName}</span>
+                <span className="truncate text-xs">{username}</span>
+              </div>
+              <ChevronsUpDown className="ml-auto size-4" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+            side={isMobile ? "bottom" : "right"}
+            align="end"
+            sideOffset={4}
+          >
+            <DropdownMenuLabel className="p-0 font-normal">
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                <Avatar className="h-8 w-8 rounded-lg">
+                  <AvatarFallback className="rounded-lg">
+                    {avatarFallback}
+                  </AvatarFallback>
+                </Avatar>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-semibold">{fullName}</span>
+                  <span className="truncate text-xs">{username}</span>
+                </div>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <Sparkles />
+                Upgrade to Pro
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <BadgeCheck />
+                Account
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <CreditCard />
+                Billing
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Bell />
+                Notifications
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <LogOut />
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  );
+}
