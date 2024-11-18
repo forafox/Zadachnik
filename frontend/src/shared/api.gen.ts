1950afef1c1441d7bfad59e44cda482866678d82
Egor Fedorov <fegor2004@gmail.com>
2024-11-18 10:10:32 +0300

    add swagger generation

---
+++ frontend/src/shared/api.gen.ts
@@ -0,0 +1,290 @@
+/* eslint-disable */
+/* tslint:disable */
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+ * ##                                                           ##
+ * ## AUTHOR: acacode                                           ##
+ * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+ * ---------------------------------------------------------------
+ */
+
+export interface SignUpRequest {
+  username: string;
+  fullName: string;
+  password: string;
+}
+
+/** Error message model */
+export interface ErrorMessage {
+  /**
+   * @format int32
+   * @example 404
+   */
+  statusCode: number;
+  /**
+   * @format date-time
+   * @example "2024-04-11T12:00:00Z"
+   */
+  timestamp: string;
+  /** @example "Resource not found" */
+  description: string;
+  /** @example "The requested resource could not be found" */
+  message: string;
+}
+
+/** JWT Response */
+export interface JwtResponse {
+  /**
+   * User ID
+   * @format int64
+   */
+  id: number;
+  /** Username */
+  username: string;
+  /** Access token */
+  accessToken: string;
+  /** Refresh token */
+  refreshToken: string;
+}
+
+export interface SignInRequest {
+  username: string;
+  password: string;
+}
+
+/** DTO for read operations on Car */
+export interface GetMeResponse {
+  /**
+   * The username of the user
+   * @example "John Doe"
+   */
+  username: string;
+  /**
+   * The ID of the user
+   * @format int64
+   * @example 1
+   */
+  id: number;
+  /**
+   * The role of the user
+   * @example "ADMIN"
+   */
+  role: "USER" | "ADMIN";
+}
+
+import type { AxiosInstance, AxiosRequestConfig, AxiosResponse, HeadersDefaults, ResponseType } from "axios";
+import axios from "axios";
+
+export type QueryParamsType = Record<string | number, any>;
+
+export interface FullRequestParams extends Omit<AxiosRequestConfig, "data" | "params" | "url" | "responseType"> {
+  /** set parameter to `true` for call `securityWorker` for this request */
+  secure?: boolean;
+  /** request path */
+  path: string;
+  /** content type of request body */
+  type?: ContentType;
+  /** query params */
+  query?: QueryParamsType;
+  /** format of response (i.e. response.json() -> format: "json") */
+  format?: ResponseType;
+  /** request body */
+  body?: unknown;
+}
+
+export type RequestParams = Omit<FullRequestParams, "body" | "method" | "query" | "path">;
+
+export interface ApiConfig<SecurityDataType = unknown> extends Omit<AxiosRequestConfig, "data" | "cancelToken"> {
+  securityWorker?: (
+    securityData: SecurityDataType | null,
+  ) => Promise<AxiosRequestConfig | void> | AxiosRequestConfig | void;
+  secure?: boolean;
+  format?: ResponseType;
+}
+
+export enum ContentType {
+  Json = "application/json",
+  FormData = "multipart/form-data",
+  UrlEncoded = "application/x-www-form-urlencoded",
+  Text = "text/plain",
+}
+
+export class HttpClient<SecurityDataType = unknown> {
+  public instance: AxiosInstance;
+  private securityData: SecurityDataType | null = null;
+  private securityWorker?: ApiConfig<SecurityDataType>["securityWorker"];
+  private secure?: boolean;
+  private format?: ResponseType;
+
+  constructor({ securityWorker, secure, format, ...axiosConfig }: ApiConfig<SecurityDataType> = {}) {
+    this.instance = axios.create({ ...axiosConfig, baseURL: axiosConfig.baseURL || "http://localhost:8080" });
+    this.secure = secure;
+    this.format = format;
+    this.securityWorker = securityWorker;
+  }
+
+  public setSecurityData = (data: SecurityDataType | null) => {
+    this.securityData = data;
+  };
+
+  protected mergeRequestParams(params1: AxiosRequestConfig, params2?: AxiosRequestConfig): AxiosRequestConfig {
+    const method = params1.method || (params2 && params2.method);
+
+    return {
+      ...this.instance.defaults,
+      ...params1,
+      ...(params2 || {}),
+      headers: {
+        ...((method && this.instance.defaults.headers[method.toLowerCase() as keyof HeadersDefaults]) || {}),
+        ...(params1.headers || {}),
+        ...((params2 && params2.headers) || {}),
+      },
+    };
+  }
+
+  protected stringifyFormItem(formItem: unknown) {
+    if (typeof formItem === "object" && formItem !== null) {
+      return JSON.stringify(formItem);
+    } else {
+      return `${formItem}`;
+    }
+  }
+
+  protected createFormData(input: Record<string, unknown>): FormData {
+    if (input instanceof FormData) {
+      return input;
+    }
+    return Object.keys(input || {}).reduce((formData, key) => {
+      const property = input[key];
+      const propertyContent: any[] = property instanceof Array ? property : [property];
+
+      for (const formItem of propertyContent) {
+        const isFileType = formItem instanceof Blob || formItem instanceof File;
+        formData.append(key, isFileType ? formItem : this.stringifyFormItem(formItem));
+      }
+
+      return formData;
+    }, new FormData());
+  }
+
+  public request = async <T = any, _E = any>({
+    secure,
+    path,
+    type,
+    query,
+    format,
+    body,
+    ...params
+  }: FullRequestParams): Promise<AxiosResponse<T>> => {
+    const secureParams =
+      ((typeof secure === "boolean" ? secure : this.secure) &&
+        this.securityWorker &&
+        (await this.securityWorker(this.securityData))) ||
+      {};
+    const requestParams = this.mergeRequestParams(params, secureParams);
+    const responseFormat = format || this.format || undefined;
+
+    if (type === ContentType.FormData && body && body !== null && typeof body === "object") {
+      body = this.createFormData(body as Record<string, unknown>);
+    }
+
+    if (type === ContentType.Text && body && body !== null && typeof body !== "string") {
+      body = JSON.stringify(body);
+    }
+
+    return this.instance.request({
+      ...requestParams,
+      headers: {
+        ...(requestParams.headers || {}),
+        ...(type ? { "Content-Type": type } : {}),
+      },
+      params: query,
+      responseType: responseFormat,
+      data: body,
+      url: path,
+    });
+  };
+}
+
+/**
+ * @title Information systems course
+ * @version 1.0.0
+ * @baseUrl http://localhost:8080
+ *
+ * Sample API
+ */
+export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDataType> {
+  api = {
+    /**
+     * @description Registers a new user with provided details and generates JWT token
+     *
+     * @tags Authorization and Registration
+     * @name Register
+     * @summary User registration
+     * @request POST:/api/auth/register
+     */
+    register: (data: SignUpRequest, params: RequestParams = {}) =>
+      this.request<JwtResponse, ErrorMessage>({
+        path: `/api/auth/register`,
+        method: "POST",
+        body: data,
+        type: ContentType.Json,
+        format: "json",
+        ...params,
+      }),
+
+    /**
+     * @description Refreshes JWT token based on provided refresh token
+     *
+     * @tags Authorization and Registration
+     * @name Refresh
+     * @summary Refresh token
+     * @request POST:/api/auth/refresh
+     */
+    refresh: (data: string, params: RequestParams = {}) =>
+      this.request<JwtResponse, ErrorMessage>({
+        path: `/api/auth/refresh`,
+        method: "POST",
+        body: data,
+        type: ContentType.Json,
+        format: "json",
+        ...params,
+      }),
+
+    /**
+     * @description Authenticates user based on provided credentials and generates JWT token
+     *
+     * @tags Authorization and Registration
+     * @name Login
+     * @summary User login
+     * @request POST:/api/auth/login
+     */
+    login: (data: SignInRequest, params: RequestParams = {}) =>
+      this.request<JwtResponse, ErrorMessage>({
+        path: `/api/auth/login`,
+        method: "POST",
+        body: data,
+        type: ContentType.Json,
+        format: "json",
+        ...params,
+      }),
+
+    /**
+     * @description Get current user
+     *
+     * @tags User Management
+     * @name Me
+     * @summary Get current user
+     * @request GET:/api/me
+     * @secure
+     */
+    me: (params: RequestParams = {}) =>
+      this.request<GetMeResponse, GetMeResponse>({
+        path: `/api/me`,
+        method: "GET",
+        secure: true,
+        ...params,
+      }),
+  };
+}
