7bd4bf297d23035218913ad539c66ac75528299d
Egor Fedorov <fegor2004@gmail.com>
2024-11-22 08:58:45 +0300

    fix: create dialog locales

--- frontend/src/widgets/create-product-dialog/ui/dialog.tsx
+++ frontend/src/widgets/create-product-dialog/ui/dialog.tsx
@@ -39,7 +39,7 @@ export function CreateProductDialogContent() {
       <Form {...form}>
         <form onSubmit={form.handleSubmit(handleSubmit)}>
           <DialogHeader>
-            <DialogTitle>{t("create.label")}</DialogTitle>
+            <DialogTitle>{t("actions.create.title")}</DialogTitle>
           </DialogHeader>
           <div className="grid gap-4 py-4 [&>*]:grid [&>*]:gap-2">
             <FormField
@@ -83,7 +83,7 @@ export function CreateProductDialogContent() {
           </div>
           <DialogFooter>
             <Button loading={isPending} type="submit">
-              {t("create.actions.create.label")}
+              {t("actions.create.label")}
             </Button>
           </DialogFooter>
         </form>
