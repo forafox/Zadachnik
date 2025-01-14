48670cce1e8bf5ecc073186ab1bf2224c4cc1004
Egor Fedorov <fegor2004@gmail.com>
2025-01-14 19:51:44 +0300

    add title

--- frontend/src/app/routes/_authenticated/articles/$articleId.tsx
+++ frontend/src/app/routes/_authenticated/articles/$articleId.tsx
@@ -1,13 +1,18 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import * as React from "react";
-import { ArticleComments, ArticleContent, getArticleQueryOptions } from "@/entities/article";
+import {
+  ArticleComments,
+  ArticleContent,
+  getArticleQueryOptions,
+} from "@/entities/article";
 import { UserHoverCard } from "@/entities/user";
 import {
   Card,
-  CardContent, CardFooter,
+  CardContent,
+  CardFooter,
   CardHeader,
-  CardTitle
+  CardTitle,
 } from "@/shared/components/ui/card.tsx";
 
 export const Route = createFileRoute("/_authenticated/articles/$articleId")({
@@ -24,7 +29,7 @@ function RouteComponent() {
     <Card>
       <CardHeader>
         <div>
-        <UserHoverCard user={article.author} />
+          Author: <UserHoverCard user={article.author} />
         </div>
       </CardHeader>
       <CardContent>
